ced5d2835dda348d7371ab7bd411f7e3ae711205
Hyper <arslanburak28@hotmail.com>
2022-02-08 15:16:34 +0300

    added custom validation and error message

--- src/modules/user/register/RegisterInput.ts
+++ src/modules/user/register/RegisterInput.ts
@@ -1,10 +1,11 @@
 import { Length, IsEmail } from "class-validator";
 import { Field, InputType } from "type-graphql";
+import { isEmailAlreadyExist } from "./isEmailAlreadyExist";
 
 @InputType()
 export class RegisterInput {
     @Field()
-    @Length(2,255)
+    @Length(2,255,{message:"must be around 2 and 255 characters"})
     firstName:string
 
     @Field()
@@ -13,6 +14,7 @@ export class RegisterInput {
 
     @Field()
     @IsEmail()
+    @isEmailAlreadyExist({message:"email already in use"})
     email:string
 
     @Field()
